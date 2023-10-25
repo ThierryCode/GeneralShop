0e321264294f025d8c4a681006e080654abc4d6e
ThierryCode <tchithierry@gmail.com>
2023-10-25 13:26:04 +0300

    Modification des packages json

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
-
+//import react rooter dom
+import {BrowserRouter as Router, Routes, Route } from "react-router-dom"
+//import pages
+import Home from ".pages/Home"
 const App = () => {
   return <div className='overflow-hidden'>react app</div>;
 };
